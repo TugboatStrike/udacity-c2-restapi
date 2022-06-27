3a33235fa68259c3b062d00d8e6a8d3e6de4f740
Byron Meals <bmeals87@gmail.com>
2022-06-27 11:49:33 -0600

    Added dotenv and changed creds

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,22 +1,22 @@
 export const config = {
   "dev": {
-    "username": "udagrambdmdev",
-    "password": "$YaLm2Mi2%Ft7tE*tqVY",
-    "database": "udagrambdmdev",
-    "host": "udagrambdmdev.csvhsefgserk.us-east-1.rds.amazonaws.com",
-    "dialect": "postgres",
-    "aws_region": "us-east-1",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram924979669534"
+    "username": process.env.DB_DEV_USERNAME,
+    "password": process.env.DB_DEV_PASSWORD,
+    "database": process.env.DB_DEV_DATABASE,
+    "host": process.env.DB_DEV_HOST,
+    "dialect": process.env.DB_DEV_DIALECT,
+    "aws_region": process.env.DB_DEV_AWS_REGION,
+    "aws_profile": process.env.DB_DEV_AWS_PROFILE,
+    "aws_media_bucket": process.env.DB_DEV_AWS_BUCKET
   },
   "jwt": {
-    "secret": "helloworld"
+    "secret": process.env.JWT_SECRET
   },
   "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+    "username": process.env.DB_PROD_USERNAME,
+    "password": process.env.DB_PROD_PASSWORD,
+    "database": process.env.DB_PROD_DATABASE,
+    "host": process.env.DB_PROD_HOST,
+    "dialect": process.env.DB_PROD_DIALECT
   }
 }
