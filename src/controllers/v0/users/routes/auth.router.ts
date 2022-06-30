c584e250f826d4c1b228f19ae1ec8450d41a2bc2
Byron Meals <bmeals87@gmail.com>
2022-06-30 00:03:53 -0600

    Added auth changes per course

--- src/controllers/v0/users/routes/auth.router.ts
+++ src/controllers/v0/users/routes/auth.router.ts
@@ -15,18 +15,18 @@ const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
     //@TODO Use Bcrypt to Generated Salted Hashed Passwords
-    return "NotYetImplemented"
-    //const rounds = 10;
-    //const salt = await bcrypt.genSalt(rounds);
-    //const hash = await bcrypt.hash(plainTextPassword, salt);
-    //return hash;
+    //return "NotYetImplemented"
+    const rounds = 10;
+    const salt = await bcrypt.genSalt(rounds);
+    const hash = await bcrypt.hash(plainTextPassword, salt);
+    return hash;
 
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
     //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
-    return true
-    //return await bcrypt.compare(plainTextPassword, hash);
+    //return true
+    return await bcrypt.compare(plainTextPassword, hash);
 }
 
 function generateJWT(user: User): string {
@@ -37,10 +37,10 @@ function generateJWT(user: User): string {
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
     //console.warn("auth.router not yet implemented, you'll cover this in lesson 5")
-    return next();
-    //if (!req.headers || !req.headers.authorization){
-   //     return res.status(401).send({ message: 'No authorization headers.' });
-    //}
+    //return next();
+    if (!req.headers || !req.headers.authorization){
+        return res.status(401).send({ message: 'No authorization headers.' });
+    }
     
 
     const token_bearer = req.headers.authorization.split(' ');
